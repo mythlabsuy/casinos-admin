000b3d56f0248506288b3fe07386fbf3daa33010
Brian Rodriguez <brodriguez@mythologylabs.com.uy>
2024-11-27 17:13:23 -0300

    fix: error message on axios

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -103,14 +103,3 @@ export type SystemUser = {
   premises: Premise[];
   disabled: boolean;
 }
-
-export class CustomError extends Error {
-  statusCode: number;
-
-  constructor(message: string, statusCode: number) {
-    super(message); 
-    this.statusCode = statusCode;
-    this.name = this.constructor.name; 
-    Object.setPrototypeOf(this, CustomError.prototype); // Ensure instanceof works correctly
-  }
-}
